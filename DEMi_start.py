0b22fc589f650048581578f5517042990c51f1cf
Rafi Schulman <schulmanr-1287@login-landau02.c.nygenome.org>
2017-07-07 15:50:20 -0400

    Added Main wrapper. Added generator to stop reading fastq into memory.

--- DEMi_start.py
+++ DEMi_start.py
@@ -5,66 +5,79 @@ import os
 Take inputs: Fastq file and Barcodes File
 Read files into usable data structures
 '''
-fastq_handle=sys.argv[1]
-barcodes_handle=sys.argv[2]
+def arg_parse():
+    fastq_handle=sys.argv[1]
+    barcodes_handle=sys.argv[2]
 
-output_directory=sys.argv[3]
-if output_directory[-1]!="/":
-    output_director+="/"
-
-#Process Barcodes: Into  a List and Dictionary
-barcodes_file=open(barcodes_handle)
-barcodes_file_lines=barcodes_file.readlines()
-barcodes_file.close()
-
-barcode_list=[]
-barcode_dicts={}
-
-
-for barcode in barcodes_file_lines:
-    barcode_list.append(barcode.strip("\n"))
-    barcode_dicts[barcode.strip("\n")]=[]
-
-
-fastq_file=open(fastq_handle)
-fastq_lines=fastq_file.readlines()
-fastq_file.close()
-
-total_reads=len(fastq_lines)/4
-
-matched_reads=[]
-unmatched_reads=[]
-
-
-read_starts=[]
-cut_sites=[]
-
-for index in range(total_reads):
-
-    raw_read_line=fastq_lines[1+4*index]
-    raw_read=raw_read_line.strip("\n")
-
-    read_start=raw_read[0:6]
-    if read_start in barcode_list:
-        barcode_dicts[read_start].append(raw_read)
-        matched_reads.append(raw_read)
-    else: 
-        unmatched_reads.append(raw_read)
+    output_directory=sys.argv[3]
+    if output_directory[-1]!="/":
+        output_directory+="/"
     
-    cut_site=raw_read[6:10]
-    if cut_site not in cut_sites:
-        cut_sites.append(cut_site)
+    return(fastq_handle,barcodes_handle,output_directory)
 
-for each_barcode in barcode_dicts:
-    barcode_file=open(output_directory+each_barcode,"w")
-    for assigned_reads in barcode_dicts[each_barcode]:
-        barcode_file.write(assigned_reads+"\n")
+def load_barcodes(barcodes):
+    barcodes_file=open(barcodes)
+    barcodes_file_lines=barcodes_file.readlines()
+    barcodes_file.close()
+
+    barcode_list=[]
+
+    for barcode in barcodes_file_lines:
+        barcode_list.append(barcode.strip("\n"))
+
+
+    return barcode_list
+
+def fastq_generator(fastq):
+    fastq_file=open(fastq)
+    read_set=[]
+    for index, lines in enumerate(fastq_file):
+        line=index+1
+        read_set.append(lines)
+        if line%4==0:
+            yield read_set
+            read_set=[]
+
+def get_read_start(four_line_read):
+    read_start=four_line_read[1][0:6]
+    return read_start    
+
+def check_for_barcode(read_start,barcode_list):
+
+    if read_start in barcode_list:
+        read_result=read_start
+    else: 
+        read_result="unmatched"
+    return read_result
+
+def write_to_file(out_dir,write_barcode,full_read_list):
+    full_read=""
+    for line in full_read_list:
+        full_read+=line
+    full_read=full_read.strip("\n")
+    print full_read
+    barcode_file=open(out_dir+write_barcode,"a")
+    barcode_file.write(full_read)
     barcode_file.close()
 
 
-barcode_file=open(output_directory+"unmatched","w")
-for reads in unmatched_reads:
-    barcode_file.write(reads+"\n")
-barcode_file.close()
+def main():
+    
+    fastq_file, barcodes_file, out_dir=arg_parse()
+    
+    if not os.path.isdir(out_dir):
+        os.system("mkdir "+out_dir)
 
-print cut_sites
+    barcodes=load_barcodes(barcodes_file)
+    
+    fastq_items=fastq_generator(fastq_file)
+   
+    counter=0 
+
+    for full_reads in fastq_items:
+        full_read=next(fastq_items)
+        read_start=get_read_start(full_read)
+        barcode_match=check_for_barcode(read_start,barcodes)
+        write_to_file(out_dir,barcode_match,full_read)
+
+main()
