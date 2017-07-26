50b66729d1cbdac5f15e69ff30e7e311e50c4554
Rafi Schulman <schulmanr-1287@login-landau02.c.nygenome.org>
2017-07-26 14:03:59 -0400

    Added R2 reading, checking, and writing. Opens and closes barcode files once.

--- DEMi_start.py
+++ DEMi_start.py
@@ -7,13 +7,27 @@ Take inputs: Fastq file and Barcodes File
 Read files into usable data structures
 '''
 def arg_parse():
-    fastq_handle=sys.argv[1]
-    barcodes_handle=sys.argv[2]
 
-    output_directory=sys.argv[3]
 
-    argument_list=[fastq_handle,barcodes_handle,output_directory]
-    
+    #check for help request arguments 
+    help_requests=["-h","--h","-help","--help"]
+
+    help_check=set(sys.argv).intersection(set(help_requests))
+    if len(help_check)>0:
+        print "help message"
+        print "Arg1: fastq, Arg2: barcodes, Arg3: output directory"
+        sys.exit()
+
+    if len(sys.argv)<4:
+        print "Error: Not enough arguments"
+        sys.exit()
+
+    R1fastq_handle=sys.argv[1]
+    R2fastq_handle=sys.argv[2]
+    barcodes_handle=sys.argv[3]
+    output_directory=sys.argv[4]
+
+       
     #Set default barcode length, adjust if argument given    
     BC_length=6
     if "-barcode_length" in sys.argv:
@@ -27,17 +41,10 @@ def arg_parse():
     
     BC_length=int(BC_length)
 
-   
-    #check for help request arguments 
-    help_requests=["-h","--h","-help","--help"]
-    help_check=set(argument_list).intersection(set(help_requests))
-    if len(help_check)>0:
-        print "help message"
-        print "Arg1: fastq, Arg2: barcodes, Arg3: output directory"
-        sys.exit()
+    argument_file_list=sys.argv[1:4]
 
     #Make sure all file/folder arguments are valid paths
-    for argument in argument_list:
+    for argument in [R1fastq_handle,R2fastq_handle,barcodes_handle,output_directory]:
         if os.path.exists(argument)==False:
             print "error- file : ", argument
             print "no such path!!"
@@ -47,7 +54,7 @@ def arg_parse():
     if output_directory[-1]!="/":
         output_directory+="/"
     
-    return(fastq_handle,barcodes_handle,output_directory,BC_length)
+    return([R1fastq_handle,R2fastq_handle],barcodes_handle,output_directory,BC_length)
 
 def load_barcodes(barcodes):
     barcodes_file=open(barcodes)
@@ -62,26 +69,39 @@ def load_barcodes(barcodes):
 
     return barcode_list
 
-def fastq_generator(fastq):
-    fastq_file=open(fastq)
+def fastq_generator(R1_fastq,R2_fastq):
+    R1_fastq_file=open(R1_fastq)
+    R2_fastq_file=open(R2_fastq)
+
     read_description=collections.namedtuple("read_set","ID sequence plus quality")
-    read_set=[]   
     
-    #FIX THIS
-    this_quality="start" 
+    while True:
 
-    while this_quality:
-        this_ID=fastq_file.readline()
-        this_sequence=fastq_file.readline()
-        this_plus=fastq_file.readline()
-        this_quality=fastq_file.readline()
+        R1_ID=R1_fastq_file.readline()
+        R2_ID=R2_fastq_file.readline()    
+            
+        if len(R1_ID)==0:
+            break
+        
+        R1_sequence=R1_fastq_file.readline()
+        R2_sequence=R2_fastq_file.readline()
+        R1_plus=R1_fastq_file.readline()
+        R2_plus=R2_fastq_file.readline()
+        R1_quality=R1_fastq_file.readline()
+        R2_quality=R2_fastq_file.readline()
 
-        read=read_description(ID=this_ID,sequence=this_sequence,plus=this_plus,quality=this_quality)
-        yield read
-         
+        R1_read=read_description(ID=R1_ID,sequence=R1_sequence,plus=R1_plus,quality=R1_quality)
+        R2_read=read_description(ID=R2_ID,sequence=R2_sequence,plus=R2_plus,quality=R2_quality)
+
+        if R1_ID.split(" ")[0]!=R2_ID.split(" ")[0]:
+            raise Exception("R1 and R2 have non-matching IDs: \nR1: "+R1_ID+"\nR2: "+R2_ID)
+
+        yield R1_read, R2_read
+
+    R1_fastq_file.close()
+    R2_fastq_file.close()         
 
 def get_read_start(four_line_read,BClength):
-
     read_start=four_line_read.sequence[0:BClength]
     return read_start    
 
@@ -93,31 +113,48 @@ def check_for_barcode(read_start,barcode_list):
         read_result="unmatched"
     return read_result
 
-def write_to_file(out_dir,barcode_filename,full_read_list):
-    full_read=""
-    for line in full_read_list:
-        full_read+=line
 
-    barcode_file=open(out_dir+barcode_filename,"a")
-    barcode_file.write(full_read)
-    barcode_file.close()
 
+def barcodefile_dict_maker(barcodes,out_dir):
+    R1_barcode_dict={}
+    R2_barcode_dict={}
+
+    for each_barcode in barcodes:
+        R1_barcode_dict[each_barcode]=open(out_dir+"R1_"+each_barcode,"w")
+        R2_barcode_dict[each_barcode]=open(out_dir+"R2_"+each_barcode,"w")
+        
+    
+    R1_barcode_dict["unmatched"]=open(out_dir+"R1_unmatched","w")
+    R2_barcode_dict["unmatched"]=open(out_dir+"R2_unmatched","w")
+
+    return R1_barcode_dict,R2_barcode_dict
 
 def main():
     
-    fastq_file, barcodes_file, out_dir, BC_length=arg_parse()
+    fastq_files, barcodes_file, out_dir, BC_length=arg_parse()
     
     if not os.path.isdir(out_dir):
         os.system("mkdir "+out_dir)
     barcodes=load_barcodes(barcodes_file)
+     
+    R1_barcodefile_dict,R2_barcodefile_dict=barcodefile_dict_maker(barcodes,out_dir)
     
-    fastq_items=fastq_generator(fastq_file)
 
-    for full_read in fastq_items:
-        read_start=get_read_start(full_read,BC_length)
+    for full_read in fastq_generator(fastq_files[0],fastq_files[1]):
+        R1_set=full_read[0]
+        R2_set=full_read[1]
+        read_start=get_read_start(R1_set,BC_length)
         barcode_match=check_for_barcode(read_start,barcodes)
-        write_to_file(out_dir,barcode_match,full_read)
+        R1_to_write="".join(R1_set)
+        R2_to_write="".join(R2_set)
+        R1_barcodefile_dict[barcode_match].write(R1_to_write)
+        R2_barcodefile_dict[barcode_match].write(R1_to_write)
 
+    for barcodefiles in R1_barcodefile_dict.keys():
+        R1_barcodefile_dict[barcodefiles].close()
+
+    for barcodefiles in R2_barcodefile_dict.keys():
+        R2_barcodefile_dict[barcodefiles].close()
 
 if __name__=="__main__":
     main()
